733a970513853605edeefd84fcbc6f8a68522bb1
bakschooltrz-prog <bakschooltrz@gmail.com>
2026-05-09 16:40:22 +0500

    Update botaskarova.py

--- botaskarova.py
+++ botaskarova.py
@@ -7,7 +7,7 @@ from telegram.ext import (
     MessageHandler, filters, ContextTypes, ConversationHandler
 )
 
-BOT_TOKEN  = os.environ.get("BOT_TOKEN", "ВСТАВЬ_ТОКЕН_АСКАРОВА")
+BOT_TOKEN  = os.environ.get("BOT_TOKEN", "8710464933:AAFhS409kgG6LuRhK61Fx_aYVABy8L3DoO0")
 MANAGER_ID = int(os.environ.get("MANAGER_ID", "1338569085"))
 BRANCH     = "Аскарова"
 
